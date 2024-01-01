09bc69bdb9a10f17d148f7a5da22c56c14d187ec
Joel C Raju <joel.raju@experionglobal.local>
2024-01-01 21:55:19 +0530

    added .gitignore file to ignore specfic files/folders

---
+++ src/scripts/firebase.js
@@ -0,0 +1,66 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, getDocs, doc } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
+import { getAuth,createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js'
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCrKosfpufYIc3yaL-pgrlcwhWqpfN2Rlg",
+  authDomain: "imdb-63ec7.firebaseapp.com",
+  projectId: "imdb-63ec7",
+  storageBucket: "imdb-63ec7.appspot.com",
+  messagingSenderId: "1089587640183",
+  appId: "1:1089587640183:web:12166709de392731e91372",
+  measurementId: "G-TR217WFC7K"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
+console.log(firebaseConfig)
+
+
+// const auth = app.auth();
+
+const db = getFirestore(app);
+
+// const db = firebase.firestore();
+
+
+
+async function getCities(db) {
+  console.log("inside db");
+  const citiesCol = collection(db, 'test');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  console.log(cityList);
+
+  // const docRef = ;
+
+  // const tryTest = await db.collection('test');
+  
+}
+
+getCities(db);
+
+
+const auth = getAuth();
+const email = "test@gmail.com";
+const password = "test1234";
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    
+    const user = userCredential.user;
+    console.log("created user");
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  })
