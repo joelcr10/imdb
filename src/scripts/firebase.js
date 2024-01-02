ca81f8a04fcd0f44b612d1ced998b6596967ff8d
Joel C Raju <joel.raju@experionglobal.local>
2024-01-02 09:41:43 +0530

    feat: added firebase authentication

--- src/scripts/firebase.js
+++ src/scripts/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, getDocs, doc } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
-import { getAuth,createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js'
+import { getFirestore, collection, getDocs, doc, setDoc } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
+import { getAuth,createUserWithEmailAndPassword,  signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js'
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -49,18 +49,66 @@ getCities(db);
 
 
 const auth = getAuth();
-const email = "test@gmail.com";
-const password = "test1234";
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up 
-    
-    const user = userCredential.user;
-    console.log("created user");
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  })
+
+
+
+
+  export const signUp = async () =>{
+    console.log("sign up is working");
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const username = document.getElementById("username").value;
+    try{
+
+      createUserWithEmailAndPassword(auth, email, password)
+      .then(async (userCredential) => {
+
+        await setDoc(doc(db, "users", userCredential.user.uid ), {
+          username: username,
+        });
+        window.location.href = "../Login_Page/login.html";
+        alert("Signup successfull");
+        
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(error);
+        // ..
+      })
+
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+
+  export const signIn = () =>{
+    console.log("inside lgin");
+    const email = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    console.log(email,password);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) =>{
+        
+        // storing the user credential to local storage
+        //accessing user info by fetching userId and getting the data from firestore
+
+        localStorage.setItem("userId",userCredential.user.uid);
+        localStorage.setItem("userEmail",userCredential.user.email);
+        window.location.href = "../index.html";
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(error);
+        // ..
+      })
+  }
+
+
+ 
+
+
+ 
--- src/scripts/includeNavbar&Footer.js
+++ src/scripts/includeNavbar&Footer.js
@@ -1,6 +1,34 @@
-const includeNavbar = () =>{
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, getDoc, doc, setDoc } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
+import { getAuth,createUserWithEmailAndPassword,  signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js'
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCrKosfpufYIc3yaL-pgrlcwhWqpfN2Rlg",
+  authDomain: "imdb-63ec7.firebaseapp.com",
+  projectId: "imdb-63ec7",
+  storageBucket: "imdb-63ec7.appspot.com",
+  messagingSenderId: "1089587640183",
+  appId: "1:1089587640183:web:12166709de392731e91372",
+  measurementId: "G-TR217WFC7K"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// const auth = app.auth();
+
+const db = getFirestore(app);
+
+
+const includeNavbar = async () =>{
     console.log("inside navbar");
-    fetch("../pages/navbar.html")
+    await fetch("../pages/navbar.html")
     .then(response => response.text())
     .then(data => {
       document.getElementById('navbar-container').innerHTML = data;
@@ -12,9 +40,40 @@ const includeNavbar = () =>{
 }
 
 
-const includeFooter = () =>{
+const userOrGuest = async () =>{
+  console.log("testing localstorage",localStorage.getItem("userId"));
+
+    if(localStorage.getItem("userId")=="null"){
+      console.log("is it getting inside");
+      document.getElementById("guest-user").style.display = "block"; //if the localStorage is null then display sign in 
+      document.getElementById("nav-profile").style.display = "none";   // hide the profile section if the user hasn't logged in
+      }
+    else{
+      document.getElementById("nav-profile").style.display = "block";
+
+      document.getElementById("guest-user").style.display = "none";  
+
+      const docRef = doc(db, "users", localStorage.getItem("userId"));
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+
+        console.log("Document data:", docSnap.data());
+        const userDoc = docSnap.data();
+        console.log(userDoc.username);
+        document.getElementById("nav-username").innerText = userDoc.username;
+
+      } else {
+        // docSnap.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    }
+}
+
+
+const includeFooter = async () =>{
     console.log("inside footer");
-    fetch("../pages/footer.html")
+    await fetch("../pages/footer.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-container').innerHTML = data;
@@ -25,9 +84,19 @@ const includeFooter = () =>{
 }
 
 
+export const signOut = () =>{
+  console.log("inside sign out");
+  localStorage.setItem("userId",null);
+  location.reload();
+}
 
-window.onload = function() {
+
+window.onload =async function() {
     console.log("onload");
-    includeNavbar();
-    includeFooter();
+    await includeNavbar();
+    await includeFooter();
+    await userOrGuest();
+    document.getElementById("signout-btn").onclick = function(){
+      signOut();
+    }
   };
